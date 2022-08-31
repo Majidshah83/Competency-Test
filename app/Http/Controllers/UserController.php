80352526256f63de6bb796ebf074021435b9e9d5
Majidshah83 <shahmajid508@gmail.com>
2022-08-31 12:35:57 +0500

    create user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
  use App\Http\Resources\UserResource;
   use App\Http\Resources\UserDepartmentResource;
  use App\Models\User;
+ use App\Models\Department;
  use App\Models\UserDepartment;
 
 class UserController extends Controller
@@ -15,5 +17,15 @@ class UserController extends Controller
         return UserResource::collection(User::with('departments')->get());
 
      }
-   
+    public function store(UserRequest $request)
+    {
+
+        $users=new User();
+        $users->name=$request->name;
+        $users->email=$request->email;
+        $users->save();
+        $profile = Department::find(1);
+        $profile->departments()->attach($users);
+        return $profile;
+    }
 }
